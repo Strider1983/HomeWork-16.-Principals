c895d34e34a038d6fd7bf8e1f4bd849f1b2d6421
Strider <mezentsev.k@gmail.com>
2023-10-16 19:46:07 +0800

    создан абстрактный класс транспорт

---
+++ src/Transport/Bicycle.java
@@ -0,0 +1,11 @@
+package Transport;
+
+public class Bicycle {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/Transport/Transport.java
@@ -0,0 +1,6 @@
+package Transport;
+
+public abstract class Transport {
+    public String modelName;
+    public int wheelsCount;
+}
