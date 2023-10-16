6c59a518a52a76470d6281e909b8fb5af19bcfd1
Strider <mezentsev.k@gmail.com>
2023-10-16 20:15:35 +0800

    создан класс моторный транспорт, скорректировано наследование

--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -1,12 +1,10 @@
 package Transport;
 
-public class Car extends Transport {
+public class Car extends EngineTransport {
 
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
 
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
+
 }
---
+++ src/Transport/EngineTransport.java
@@ -0,0 +1,10 @@
+package Transport;
+
+public abstract class EngineTransport extends Transport{
+    public EngineTransport(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
